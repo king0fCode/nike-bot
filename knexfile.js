88e5358758913fd1f043c749f3eb51d7cc955cfb
codedays <kingfcodes@gmail.com>
2021-05-10 09:29:05 +0400

    s

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 require('dotenv-flow').config();
 
 module.exports = {
-  local: {
+  dev: {
     client: 'pg',
     connection: {
       host: process.env.DB_HOST,
@@ -12,25 +12,6 @@ module.exports = {
     },
     useNullAsDefault: true,
     debug: true,
-    migrations: {
-      directory: 'db/migrations',
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      directory: 'db/seeds'
-    }
-  },
-  docker: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USERNAME,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD
-    },
-    useNullAsDefault: true,
-    debug: false,
     migrations: {
       directory: 'db/migrations',
       tableName: 'knex_migrations'
